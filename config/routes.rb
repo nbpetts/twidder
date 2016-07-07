4d0456dda0c774f4d38a5139c2050fe3479ae5ad
Nathan Petts <nbpetts@gmail.com>
2016-07-07 17:02:03 -0500

    added signup form and some tests. Just before 7.4 in Hartl

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
   
-
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
