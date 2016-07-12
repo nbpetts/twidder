9186e9934fbddadb3a2c1beb31560f3fab3460eb
Nathan Petts <nbpetts@gmail.com>
2016-07-12 13:52:06 -0500

    Impliment Basic Login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
+      log_in @user
       flash[:success] = "Welcome to Twidder, Home of Brand New Ideas"
   		redirect_to @user
   	else
